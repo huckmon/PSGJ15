3fbc47a739eb78e51b6ed79fb3a2ae4c0de540b9
Huckmon <45248261+huckmon@users.noreply.github.com>
2024-07-24 12:50:23 +0930

    Started transistion system skeleton
    Started base transition system

--- Godot Project/Scenes/Production Scene/production_simpleton.gd
+++ Godot Project/Scenes/Production Scene/production_simpleton.gd
@@ -1,7 +1,14 @@
 extends Node
 
+# Following vars store the global positions for items in the production scene to spawn the item versions in the same place as the canvas scenes
 var canvas_knife_globalpos
 var canvas_bowl_globalpos
 
 var canvas_red_potion_globalpos
 var canvas_blue_potion_globalpos
+
+# Following is storeing the posisitions of components needed for the transistion system
+
+var production_scene_globalpos # might be useless bc the node is at 0,0
+var sales_scene_globalpos # same as above comment
+
